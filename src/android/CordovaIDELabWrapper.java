8f4d3c8da177340321f15058610727716cf25058
mariusz-strawinski <40897193+mariusz-strawinski@users.noreply.github.com>
2018-12-12 13:03:28 +0100

    Update CordovaIDELabWrapper.java

--- src/android/CordovaIDELabWrapper.java
+++ src/android/CordovaIDELabWrapper.java
@@ -14,47 +14,24 @@ import org.json.JSONObject;
  */
 public class CordovaIDELabWrapper extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("idelab")) {
-			
-			//new IntentAppointment(this, getPatient()).startAppointment();
-			
-			
-			
-			
-			
-            //String message = args.getString(0);
-            //this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
-    }
-/*
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
-	
-*/
+ private static final String TAG = "CordovaIDELabWrapper";
 
-	@Override 
-	public void onActivityResult(int requestCode, int resultCode, Intent data) { 
-		/*IntentAppointmentResult result = IntentAppointment.onActivityResult(requestCode, resultCode, data); 
-		if(result != null) { 
-			if(result.getCode() == RESULT_CANCELED) { 
-				Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show(); 
-				} 
-			 else { 
-				Toast.makeText(this, "Success: " + result.getCode(), Toast.LENGTH_LONG).show(); 
-				} 
-			} 
-		 else { 
-			super.onActivityResult(requestCode, resultCode, data); 
-			} 
-	*/	
-	}
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+
+    Log.d(TAG, "Initializing MyCordovaPlugin");
+  }
+
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+    if(action.equals("idelab")) {
+      String phrase = args.getString(0);
+      // Echo back the first argument
+      Log.d(TAG, phrase);
+    } else if(action.equals("getDate")) {
+      // An example of returning data back to the web layer
+      final PluginResult result = new PluginResult(PluginResult.Status.OK, (new Date()).toString());
+      callbackContext.sendPluginResult(result);
+    }
+    return true;
+}
 }
