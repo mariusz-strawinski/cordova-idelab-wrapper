baa6010038fb2f6870b7d4bd5d5d9a262ff8f1dd
mariusz-strawinski <40897193+mariusz-strawinski@users.noreply.github.com>
2018-12-12 12:08:39 +0100

    Create CordovaIDELabWrapper.js

---
+++ www/CordovaIDELabWrapper.js
@@ -0,0 +1,15 @@
+var exec = require('cordova/exec');
+
+var PLUGIN_NAME = 'CordovaIDELabWrapper';
+
+var CordovaIDELabWrapper = {
+	
+	send: function (name, birthday, ) {
+		//exec(success, error, 'CordovaIDELabWrapper', 'idelab', [name]);
+		alert('TEST');
+		}
+	
+	}
+
+
+module.exports = CordovaIDELabWrapper;
